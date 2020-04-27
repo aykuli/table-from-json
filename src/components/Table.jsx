bc36cff7fc6bec7e749153da08b4d806f2af3231
aykuli <aykuli@ya.ru>
2020-04-27 19:28:49 +0700

    feat: change divs-cells on input

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,21 +1,28 @@
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable no-unused-vars */
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import table1 from '../mock-json-tables/table-1.json';
-import tableLevelsMapping from '../utils/table-levels-map-1';
 
 import './Table.scss';
 import { SPAN_WIDTH, SPAN_HEIGHT } from '../constantas';
 
-const tableLevelsMap = tableLevelsMapping(table1);
-console.log(tableLevelsMap);
-
 const Tree = props => {
   const { items, level } = props;
 
+  const [history, setHistory] = useState([]);
+  const [stepNumb, setStepNumb] = useState(0);
+
   if (!items || typeof items !== 'object') return null;
+
+  const handleChange = (e, color) => {
+    console.log(color);
+    console.log('clicked: ', e.target);
+  };
+  const handleFocus = () => {
+    console.log('focused');
+  };
 
   return (
     <>
@@ -23,22 +30,30 @@ const Tree = props => {
         const { VerticalSpan, Color, Value, Children } = item;
         const isChildrenExist = Children.length ? 1 : 0;
 
-        const height = SPAN_WIDTH * VerticalSpan;
+        const height = SPAN_HEIGHT * VerticalSpan;
+        const width = isChildrenExist ? SPAN_WIDTH * Children.length : SPAN_WIDTH;
 
         return isChildrenExist ? (
           <div className="span__parent" key={Value}>
-            <div className="span__child" style={{ backgroundColor: Color, border: '1px solid black', height }}>
-              {Value}
-            </div>
+            <input
+              className="span__child"
+              style={{ backgroundColor: Color, height, width }}
+              onChange={e => handleChange(e, Color)}
+              tabIndex={0}
+              value={Value}
+            />
             <div className="span__child" style={{ flexGrow: 1 }}>
               <Tree items={Children} level={level + 1} />
             </div>
           </div>
         ) : (
-          <div key={Value} style={{ backgroundColor: Color, width: SPAN_WIDTH }} className="span__last">
-            {Value}
-            {console.log('render last')}
-          </div>
+          <input
+            key={Value}
+            style={{ backgroundColor: Color, width: SPAN_WIDTH, minHeight: SPAN_HEIGHT }}
+            className="span__last"
+            value={Value}
+            onChange={handleChange}
+          />
         );
       })}
     </>
--- src/constantas.js
+++ src/constantas.js
@@ -1,2 +1,2 @@
 export const SPAN_WIDTH = 50;
-export const SPAN_HEIGHT = 50;
+export const SPAN_HEIGHT = 40;
